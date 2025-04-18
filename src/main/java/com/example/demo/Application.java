0ee42da4d55cd2b63abc5714f4d03748a567d03a
supriya <supriya2110>
2025-04-18 23:12:15 +0530

    modified

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -10,6 +10,9 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 		int a=10;
 		int b=20;
+                int c=30;
+                int d=a+b+c;
+                System.out.println(d);
 	}
 
 }
